da0aa31a4859d65d54a0910b1cbf7b52dc613f01
Front-biger <626497248@qq.com>
2019-05-27 11:17:14 +0800

    登录页修改

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,36 +1,105 @@
 <template>
-  <div>
-     <input type="text" v-model="username"/>
-     <input type="password" v-model="password"/>
-     <button @click="login">
-        登录
-     </button>
+  <div class="from">
+    <!-- <el-input
+      placeholder="请输入用户名"
+      v-model="username"
+      clearable
+    >
+    </el-input>
+
+    <el-input
+      placeholder="请输入密码"
+      v-model="password"
+      clearable
+    >
+    </el-input> -->
+    <!-- <input
+      type="text"
+      v-model="username"
+    /> -->
+    <!-- <input
+      type="password"
+      v-model="password"
+    /> -->
+    <!-- <button @click="login">
+      登录
+    </button> -->
+    <el-form
+      class="elform"
+      label-width="80px"
+      :model="formLabelAlign"
+    >
+      <el-form-item label="用户名">
+        <el-input v-model="formLabelAlign.username"></el-input>
+      </el-form-item>
+      <el-form-item
+        label="密码"
+        prop="pass"
+      >
+        <el-input
+          type="password"
+          v-model="formLabelAlign.password"
+          auto-complete="off"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          type="primary"
+          @click="login"
+          class="denglu"
+          size="medium"
+          width="90%"
+        >登录</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 <script>
-  export default {
-     name:"login",
-     data(){
-       return{
-         username:"t001",
-         password:""
-       }
-     },
-     methods:{
-       login(){
-          this.$ajax.get(`http://124.172.243.65:8096/xtz/account/login?username=${this.username}&password=${this.password}`).then(res=>{
-              let data = JSON.parse(res.data);
-              if(data.code == 200){
-                 localStorage.setItem("accessToken", data.accessToken);
-                localStorage.setItem("userTypeId",data.userTypeId);
-                localStorage.setItem("schoolId",data.schoolId);
-                this.$router.push("/talentSearch");
-             }else{
-                 alert('账号或密码不对')
-             }
+export default {
+  name: "login",
+  data () {
+    return {
+      formLabelAlign: {
+        username: '',
+        password: '',
+      },
+      //   username: "t001",
+      //   password: ""
+    }
+  },
+  methods: {
+    login () {
+      this.$ajax.get(`http://124.172.243.65:8096/xtz/account/login?username=${this.formLabelAlign.username}&password=${this.formLabelAlign.password}`).then(res => {
+        let data = JSON.parse(res.data);
+        if (data.code == 200) {
+          localStorage.setItem("accessToken", data.accessToken);
+          localStorage.setItem("userTypeId", data.userTypeId);
+          localStorage.setItem("schoolId", data.schoolId);
+          this.$router.push("/talentSearch");
+        } else {
+          alert('账号或密码不对')
+        }
 
-          })
-       }
-     }
+      })
+    }
   }
+}
 </script>
+<style scoped>
+.from {
+  width: 500px;
+  /* height: 300px; */
+  /* border: 1px solid rgb(99, 98, 98); */
+  margin: 200px auto;
+}
+.elform {
+  margin: 50px 10px;
+}
+.el-input {
+    
+    width: 80%;
+}
+ .el-button {
+  width: 80%;
+}
+</style>
---
+++ src/components/loginTwo.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <!-- <input type="text" v-model="username"/>
+     <input type="password" v-model="password"/>
+     <button @click="login">
+        登录
+     </button> -->
+  </div>
+</template>
+<script>
+export default {
+  name: "login",
+  data () {
+    return {
+      theRequest: {}
+    }
+  },
+  created () {
+
+  },
+  mounted(){
+         this.login()
+  },
+  methods: {
+    login () {
+      var url = this.$route.query; //获取url中"?"符后的字串  
+      
+      console.log(url);
+        this.theRequest = url
+      // var theRequest = new Object();
+      // console.log( theRequest );
+      // if (url.indexOf("?") != -1) {
+      //   var str = url.substr(1);
+      //   strs = str.split("&");
+      //   for (var i = 0; i < strs.length; i++) {
+      //     this.theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+      //   }
+      // }
+      console.log( this.theRequest );
+        console.log(this.theRequest.naughty);
+      var token = localStorage.getItem("accessToken");
+      if (this.theRequest.naughty == undefined && this.theRequest.veryStrong == undefined && token == null) {
+       
+        //未登录 跳转到登录 	
+      //   window.location.href = "http://120.25.66.101:7777/auth/login"
+      }
+
+      //跳转方登录   目标方未登录则异步登录
+      if (this.theRequest.naughty != undefined && this.theRequest.veryStrong != undefined && token == null) {
+        //没有时间戳跳转到
+        if (this.theRequest.v == undefined) {
+         //  window.location.href = "http://120.25.66.101:7777/auth/login"
+        } else {
+         //  
+         console.log(this.theRequest.naughty);
+         
+           this.$ajax.get(`http://192.168.1.102:8096/new/login?naughty=${this.theRequest.naughty}&veryStrong=${this.theRequest.veryStrong}&v=${this.theRequest.v}`).then(res=>{
+              let data = JSON.parse(res.data);
+              console.log(data);
+              
+              console.log(data.msg);
+              
+              if(data.msg == "登录成功"){
+               //保存登录信息
+                // sessionStorage.setItem("token", data.data.accessToken);
+                  localStorage.setItem("accessToken", data.data.accessToken);
+                localStorage.setItem("userTypeId",data.data.userTypeId);
+                localStorage.setItem("schoolId",data.data.schoolId);
+                this.$router.push("/talentSearch");
+
+               //  console.log(data.accessToken);
+                
+                // //成功跳转到指定页
+                // if (this.theRequest.toUrl != undefined) {
+                //   // window.location.href = this.theRequest.toUrl
+                // } else {
+                //   //跳转到首页
+                //   // window.location.href = "http://120.25.66.101:7777"
+                // }
+             }else{
+                 //首页或登录页
+               //  window.location.href = "http://120.25.66.101:7777/auth/login"
+             }
+
+          })
+        }
+      }
+
+
+      //跳转方和目标方 都登录  转到目标url
+      if (this.theRequest.naughty != undefined && this.theRequest.veryStrong != undefined && token != null) {
+       this.$router.push("/talentSearch");
+      }
+      //跳转方为登录 目标方 都登录  转到目标url
+      if (this.theRequest.naughty == undefined && this.theRequest.veryStrong == undefined && token != null) {
+       this.$router.push("/talentSearch");
+      }
+    }
+  }
+}
+</script>
